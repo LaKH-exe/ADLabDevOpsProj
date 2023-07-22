d2f1291e3a61ee39e67f8c272faffc0ddb636a63
LaKH-exe <31774612+LaKH-exe@users.noreply.github.com>
2023-07-22 14:09:55 +0300

    add nsg and nsg association

--- provider.tf
+++ provider.tf
@@ -71,6 +71,43 @@ resource "azurerm_network_interface" "nic" {
     subnet_id = azurerm_virtual_network.main_net.subnet[0].id
   }
 
+resource "azurerm_network_security_group" "main-nsg" {
+  name                = "main-nsg"
+  location            = var.location
+  resource_group_name = var.rg
+  # RDP
+  security_rule {
+    name                       = "Allow-RDP"
+    priority                   = 100
+    direction                  = "Inbound"
+    access                     = "Allow"
+    protocol                   = "Tcp"
+    source_port_range          = "*"
+    destination_port_range     = "3389"
+    source_address_prefix      = "*"
+    destination_address_prefix = "*"
+  }
+  # WinRM
+  security_rule {
+    name                       = "Allow-WinRM"
+    priority                   = 101
+    direction                  = "Inbound"
+    access                     = "Allow"
+    protocol                   = "Tcp"
+    source_port_range          = "*"
+    destination_port_range     = "5985"
+    source_address_prefix      = "*"
+    destination_address_prefix = "*"
+  }
+}
+
+
+resource "azurerm_network_interface_security_group_association" "example" {
+  network_interface_id      = network_interface_ids.nic[var.resources_name[count.index]]
+  network_security_group_id = azurerm_network_security_group.main-nsg.id
+}
+
+
 
 resource "azurerm_windows_virtual_machine" "VMs"{
 # get the admin pass from key vault
